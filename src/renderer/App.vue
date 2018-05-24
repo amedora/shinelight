07751d4592eda66b87570cf9b5b4fa3ffdb416dc
amedora <americandragsterracing@gmail.com>
2018-05-24 16:53:27 +0900

    コマンドリストをファイルから読み込む。

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,9 +5,29 @@
 </template>
 
 <script>
-  export default {
-    name: 'shinelight'
+import { mapActions, mapState } from 'vuex'
+import settings from 'electron-settings'
+
+export default {
+  name: 'shinelight',
+  created () {
+    this.loadCommands()
+  },
+  computed: {
+    ...mapState('CommandList', {
+      commandList: 'commandList'
+    })
+  },
+  methods: {
+    ...mapActions('CommandList', {
+      initCommandList: 'init'
+    }),
+    loadCommands () {
+      const list = settings.get('command-list')
+      this.initCommandList(list)
+    }
   }
+}
 </script>
 
 <style>
