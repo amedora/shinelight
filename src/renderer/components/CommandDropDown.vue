0a3a53c24941b6c6ad4acc552d69d116ef02dcba
amedora <americandragsterracing@gmail.com>
2018-05-24 09:58:15 +0900

    new: CommandDropDown

---
+++ src/renderer/components/CommandDropDown.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dropdown" :class="{ 'is-active': isShown }">
+    <div class="dropdown-trigger" @click="toggle">
+      <input class="input is-primary" type="text" aria-haspopup="true" aria-controls="dropdown-menu">
+    </div>
+    <div class="dropdown-menu" id="dropdown-menu" role="menu">
+      <div class="dropdown-content">
+        <a v-for="(key, name) in commands" :key="key" href="#" class="dropdown-item">
+          {{ name }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'command-drop-down',
+  props: ['commands'],
+  data () {
+    return {
+      isShown: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.isShown = !this.isShown
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
