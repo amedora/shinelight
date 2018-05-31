6e2134df533080e9a3da850e9d757db4a433597f
amedora <americandragsterracing@gmail.com>
2018-05-31 14:22:50 +0900

    new: MyHeader

---
+++ src/renderer/components/MyHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar is-fixed-top is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <p>shinelight</p>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  min-height: 32px;
+  height: 32px;
+}
+</style>
