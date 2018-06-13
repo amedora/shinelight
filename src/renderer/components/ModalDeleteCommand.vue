1415f35e6b4e3c136c8a3eb12530d1ce2a66a4dc
amedora <americandragsterracing@gmail.com>
2018-06-13 12:11:16 +0900

    new: ModalDeleteCommand: コマンドの削除を確認するモーダル

---
+++ src/renderer/components/ModalDeleteCommand.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="modal"
+    :class="{ 'is-active': show }">
+    <div class="modal-background" />
+    <div class="modal-content box ">
+      <nav class="level is-mobile">
+        <div class="level-left">
+          <div class="level-item">
+            <p>{{ command.name }} を削除しますか？</p>
+          </div>
+        </div>
+        <div class="level-right">
+          <p class="level-item">
+            <a class="button is-small is-danger is-outlined">はい</a>
+          </p>
+          <p class="level-item">
+            <a
+              class="button is-small is-outlined"
+              @click="close">いいえ</a>
+          </p>
+        </div>
+      </nav>
+    </div>
+    <button
+      class="modal-close is-large"
+      @click="close"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'modal-delete-command',
+  props: ['show', 'command'],
+  methods: {
+    close () {
+      this.show = false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
