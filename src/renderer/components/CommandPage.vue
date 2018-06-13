7388470e0d1ead1dbfc594f4ec3ae3ee98ea5f77
amedora <americandragsterracing@gmail.com>
2018-06-13 12:12:07 +0900

    コマンド削除イベント -> モーダルを表示する。

--- src/renderer/components/CommandPage.vue
+++ src/renderer/components/CommandPage.vue
@@ -3,17 +3,35 @@
     <commander
       @delete-command="onDeleteCommand"
       @new-file="onNewFile" />
+    <modal-delete-command
+      :show="showModal"
+      :command="commandToDelete"
+      @close="showModal = false"/>
   </div>
 </template>
 
 <script>
 import Commander from './Commander.vue'
+import ModalDeleteCommand from './ModalDeleteCommand.vue'
+
 export default {
   components: {
-    Commander
+    Commander,
+    ModalDeleteCommand
+  },
+  data () {
+    return {
+      showModal: false,
+      commandToDelete: {
+        name: '',
+        path: ''
+      }
+    }
   },
   methods: {
     onDeleteCommand (command) {
+      this.commandToDelete = command
+      this.showModal = true
     },
     onNewFile (filePath) {
       this.$router.push({
