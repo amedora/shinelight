a5fece1326ba2af6939610d7b1205e533879a33d
amedora <americandragsterracing@gmail.com>
2018-06-15 09:21:36 +0900

    タイトルバーは閉じるボタンを持つ:
    - see https://github.com/jgthms/bulma/issues/1445

--- src/renderer/components/MyHeader.vue
+++ src/renderer/components/MyHeader.vue
@@ -3,6 +3,13 @@
     <div class="navbar-brand">
       <p class="navbar-item">shinelight</p>
     </div>
+    <div class="navbar-menu has-background-primary is-shadowless">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <a class="button is-inverted is-primary is-outlined is-small">_</a>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -18,8 +25,36 @@ export default {
   min-height: 32px;
   height: 32px;
 }
+.navbar .container {
+  display: flex;
+}
+.navbar,
+.navbar-menu,
+.navbar-start,
+.navbar-end {
+  align-items: stretch;
+  display: flex;
+  padding: 0;
+}
+.navbar-menu {
+  flex-grow: 1;
+  flex-shrink: 0;
+}
+.navbar-start {
+  justify-content: flex-start;
+  margin-right: auto;
+}
+.navbar-end {
+  justify-content: flex-end;
+  margin-left: auto;
+}
 .navbar-item {
+  display: flex;
+  align-items: center;
   height: 30px;
-  padding-left: 8px;
+}
+.navbar-item .button {
+  height: 20px;
+  width: 20px;
 }
 </style>
